53cba3746bfc39dfd2635576730b9110c0e692ba
Aryan swaroop <aryan@Aryans-MacBook-Pro.local>
2026-07-27 03:37:14 +0530

    Disable double-tap zoom, image download, right-click on images

--- script.js
+++ script.js
@@ -271,3 +271,7 @@ document.querySelectorAll('.copy-email').forEach(function(btn) {
   });
 });
 
+// Disable right-click on images + prevent drag
+document.addEventListener('contextmenu',function(e){if(e.target.tagName==='IMG')e.preventDefault();});
+document.addEventListener('dragstart',function(e){if(e.target.tagName==='IMG')e.preventDefault();});
+
